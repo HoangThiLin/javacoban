582b270e1097c8e8840b9b8a953a37db778c7610
HoangThiLin <97728010+HoangThiLin@users.noreply.github.com>
2022-05-26 00:19:48 +0700

    bai tap lap trinh
    bai tap lap trinh

--- Student.java
+++ Student.java
@@ -1,70 +1,42 @@
-package ViDu;
+package Model;
 
 import java.io.Serializable;
-import java.sql.Date;
+import java.util.Date;
 
-public class Student implements Comparable<Student>,Cloneable,Serializable     {	
-		 private String fullNmae;
-		 private Date BirthDate;
-		 private Boolean genDer;
-		 private float gpa;
-		 private int socialActivities;
-		 private static final long serialVersionUID = 1L;
-		public Student(String fullNmae, Date birthDate, Boolean genDer, float gpa, int socialActivities) {
-			this.fullNmae = fullNmae;
-			BirthDate = birthDate;
-			this.genDer = genDer;
-			this.gpa = gpa;
-			this.socialActivities = socialActivities;
-		}
-		public String getFullNmae() {
-			return fullNmae;
-		}
-		public void setFullNmae(String fullNmae) {
-			this.fullNmae = fullNmae;
-		}
-		public Date getBirthDate() {
-			return BirthDate;
-		}
-		public void setBirthDate(Date birthDate) {
-			BirthDate = birthDate;
-		}
-		public Boolean isGenDer() {
-			return genDer;
-		}
-		public void setGenDer(Boolean genDer) {
-			this.genDer = genDer;
-		}
-		public float getGpa() {
-			return gpa;
-		}
-		public void setGpa(float gpa) {
-			this.gpa = gpa;
-		}
-		public int getSocialActivities() {
-			return socialActivities;
-		}
-		public void setSocialActivities(int socialActivities) {
-			this.socialActivities = socialActivities;
-		}
-		@Override
-		public int compareTo(Student o) {
-			if ((this.gpa - o.gpa)>0)
-			 return 1;
-			else if ((this.gpa - o.gpa)==0) {
-				return this.socialActivities-o.socialActivities;
-				//return 0;
-				
-			}
-				
-			else
-			//return this.socialActivities-o.socialActivities;
-			return -1;
-			
-		}	
-		@Override
-		   protected Object clone() throws CloneNotSupportedException {
-		       return super.clone();
-		   }
+public class Student extends Person implements Comparable<Student>,Serializable{
+	private String StudentID;
+	private String major;
+	private Date enrolledDate;
+	public String getStudentID() {
+		return StudentID;
+	}
+	public void setStudentID(String studentID) {
+		StudentID = studentID;
+	}
+	public String getMajor() {
+		return major;
+	}
+	public void setMajor(String major) {
+		this.major = major;
+	}
+	public Date getEnrolledDate() {
+		return enrolledDate;
+	}
+	public void setEnrolledDate(Date enrolledDate) {
+		this.enrolledDate = enrolledDate;
+	}
+	public Student(String fullName, Date birthday, Boolean gender, String studentID, String major, Date enrolledDate) {
+		super(fullName, birthday, gender);
+		StudentID = studentID;
+		this.major = major;
+		this.enrolledDate = enrolledDate;
+	}
+	@Override
+	public int compareTo(Student o) {
+		
+		return 0;
+	}
+
+	
 
 }
